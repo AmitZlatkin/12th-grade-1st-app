c4c436435aef40c3745b282de3ebb46502967c3c
Amit Zlatkin <amit.zlatkin@campus.technion.ac.il>
2024-09-22 13:28:48 +0300

    added new activity for viewing the data

---
+++ app/src/main/java/com/example/grade_12_app_1/ListActivity.java
@@ -0,0 +1,81 @@
+package com.example.grade_12_app_1;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    ListView lvDogs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        db = FirebaseFirestore.getInstance();
+        lvDogs = findViewById(R.id.lvDogs);
+        GetFromDataSet();
+    }
+
+    private void SetStatus(String s) {
+        ((TextView)findViewById(R.id.tvStatus)).setText(s);
+    }
+
+
+    private void GetFromDataSet()
+    {
+        SetStatus("Reading Firebase:");
+        db.collection("Dogs")
+                //.whereEqualTo("DogColor", "Blue")
+                //.orderBy("DogName")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            SetStatus("Error: " + task.getException());
+                            return;
+                        }
+                        DataToAdapter(task);
+                    }
+                });
+    }
+    private void DataToAdapter(Task<QuerySnapshot> task) {
+        int cnt = 0;
+        String dog;
+        ArrayList<String> listItem = new ArrayList<>();
+
+
+        for (QueryDocumentSnapshot document : task.getResult()) {
+            if (document.getMetadata().isFromCache()) {
+                SetStatus("The Data is from cache. sorry");
+                return;
+            }
+            cnt++;
+            dog = document.get("DogName") +"\n"+ document.get("DogColor");
+            listItem.add(dog);
+        }
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItem);
+        lvDogs.setAdapter(adapter);
+        SetStatus("Records found: " + cnt);
+    }
+
+}
