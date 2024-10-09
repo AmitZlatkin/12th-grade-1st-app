30362ec048d9823e543455f6bfe234909c69a39a
Amit Zlatkin <amit.zlatkin@campus.technion.ac.il>
2024-10-09 18:29:37 +0300

    for some reason it forgot those two

--- app/src/main/java/com/example/grade_12_app_1/DetailsActivity.java
+++ app/src/main/java/com/example/grade_12_app_1/DetailsActivity.java
@@ -1,6 +1,10 @@
 package com.example.grade_12_app_1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,28 +16,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class DetailsActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
+    String dogName;
+    ImageView dogPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        String dogName = getIntent().getStringExtra("DogName");
-        ((TextView)findViewById(R.id.dtlDogName)).setText(dogName);
+        dogName = getIntent().getStringExtra("DogName");
+
+        dogPhoto = findViewById(R.id.DogImage);
 
         db = FirebaseFirestore.getInstance();
-        GetDataFromDB(dogName);
+        BuildQuery(dogName);
     }
+
+
 
     private void SetStatus(String msg) {
         ((TextView)findViewById(R.id.dtlStatus)).setText(msg);
     }
 
 
-    private void GetDataFromDB(String dogName) {
+    private void BuildQuery(String dogName) {
         SetStatus("Reading Firebase:");
         db.collection("Dogs")
                 .whereEqualTo("DogName",dogName)
@@ -57,20 +72,28 @@ public class DetailsActivity extends AppCompatActivity {
                 SetStatus("The Data is from cache. sorry");
                 return;
             }
-            String color = document.get("DogColor").toString();
-            ((TextView)findViewById(R.id.dtlDogColor)).setText(color);
-
-            SetStatus("Found it!");
+            DisplayDetails(document);
             return;
         }
         SetStatus("Can not find dog :(");
     }
 
-/*
+    private void DisplayDetails(QueryDocumentSnapshot document) {
+        ((TextView)findViewById(R.id.dtlDogName)).setText(dogName);
+        String color = document.get("DogColor").toString();
+        ((TextView)findViewById(R.id.dtlDogColor)).setText(color);
+
+        String photoUri = document.get("Uri").toString();
+        DownloadImage(photoUri);
+
+
+        SetStatus("Found it!");
+    }
+
+
     private void DownloadImage(String url) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-
 
         Bitmap bm = null;
         try {
@@ -80,12 +103,12 @@ public class DetailsActivity extends AppCompatActivity {
             InputStream inp = conn.getInputStream();
             BufferedInputStream bis = new BufferedInputStream(inp);
             bm = BitmapFactory.decodeStream(bis);
-            photo.setImageBitmap(bm);
+            dogPhoto.setImageBitmap(bm);
             bis.close();
             inp.close();
         } catch (IOException e) {
             SetStatus("Error getting bitmap: "+e.getMessage());
         }
     }
-*/
+
 }
--- app/src/main/java/com/example/grade_12_app_1/MainActivity.java
+++ app/src/main/java/com/example/grade_12_app_1/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.grade_12_app_1;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,12 +33,37 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
 
+    ActivityResultLauncher activityResultLauncher;
+    ImageView DogImage;
+    Bitmap bitmap;
+    Map<String, Object> dbNewData;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
+        dbNewData= new HashMap<>();
+        DogImage = findViewById(R.id.DogImage);
+
+        AddCameraListener();
     }
+
+    private void AddCameraListener()
+    {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback() {
+            @Override
+            public void onActivityResult(Object result) {
+                ActivityResult res = (ActivityResult)result;
+                if (res.getResultCode() == RESULT_OK && res.getData() != null) {
+                    bitmap = (Bitmap) res.getData().getExtras().get("data");
+                    DogImage.setImageBitmap(bitmap);
+                }
+            }
+        });
+    }
+
 
     public void Save(View view) {
         String dogName = ((EditText)findViewById(R.id.editDogName)).getText().toString();
@@ -36,13 +73,50 @@ public class MainActivity extends AppCompatActivity {
             SetStatus("Please type dog's name + color");
         }
 
-        Map<String, Object> dbNewData = new HashMap<>();
         dbNewData.put("DogName", dogName);
         dbNewData.put("DogColor", dogColor);
 
-        AddNewDocument("Dogs", dbNewData);
+        if(bitmap != null) {
+            UploadImage(dogName);
+        } else {
+            dbNewData.put("DogPhoto", "");
+            AddNewDocument("Dogs", dbNewData);
+        }
     }
 
+    private void UploadImage(String dogName) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+
+        StorageReference storageRef;
+        storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference ref = storageRef.child( "dogImages/"+dogName+".jpg");
+        UploadTask uploadTask = ref.putBytes(data);
+
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                SetStatus("Failed:"+e.getMessage());
+            }
+        })
+        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        dbNewData.put("Uri", uri.toString());
+                        AddNewDocument("Dogs", dbNewData);
+                    }
+                });
+                SetStatus("Image uploaded successfully");
+            }
+        });
+
+    }
     private void AddNewDocument(String collection, Map<String, Object> newData) {
 
         /*
@@ -76,5 +150,11 @@ public class MainActivity extends AppCompatActivity {
     public void GoToList(View view) {
         Intent i = new Intent(this, ListActivity.class);
         startActivity(i);
+    }
+
+    public void OpenCamera(View view) {
+        Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        activityResultLauncher.launch(iCamera);
+
     }
 }
